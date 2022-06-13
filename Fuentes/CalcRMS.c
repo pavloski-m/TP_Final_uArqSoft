5f5beb63fcfdaea8f6c7c8baaf68eeb4744a7531
Pablo <pablomorzan@gmail.com>
2022-06-13 00:49:03 -0300

    working c

--- Fuentes/CalcRMS.c
+++ Fuentes/CalcRMS.c
@@ -34,25 +34,38 @@ typedef union {
     };
 } Signal_t;
 
-//tabla datos de señal senoidal
+//tabla datos de señal senoidal rectificada
 uint16_t seno16[] = {0x0000,0x33a8,0x5f73,0x7cb6,0x86fd,0x7cb6,0x5f73,0x33a8,0x0000,0x33a8,0x5f73,0x7cb6,0x86fd,0x7cb6,0x5f73,0x33a8};
 
 //valor de entrada para múltiplicar la señal
 uint8_t valor = 1;
 
-    Bits_t reset_data = {.bitsData=0};
-    Bits_t enable_data = {.bitsData=0};
-    Signal_t input_data = {.signalData=0};
-    Signal_t output_data = {.signalData=0};
+Bits_t reset_data = {.bitsData=0};
+Bits_t enable_data = {.bitsData=0};
+Signal_t input_data = {.signalData=0};
+Signal_t output_data = {.signalData=0};
 
 int main (void) {
 
 	uint8_t i = 0;
+	uint8_t count = 20;
+	float mul = 1;
 
     xil_printf("-- Inicio del programa.... --\r\n");
+    xil_printf("-- Para señal de voltaje 220V presione A \r\n-- Para voltaje 110V presione L \r\n");
     valor = inbyte();
-    xil_printf("Valor ingresado: %d\r\n", valor);
 
+    if(valor=='a'){
+    	mul=1;
+    }
+    else {mul=0.5;
+    }
+
+    xil_printf("Valor ingresado: %d V\r\n", (uint16_t)mul*220);
+
+    FILAVG_mWriteReg(XPAR_FILAVG_0_S_AXI_BASEADDR, FILAVG_S_AXI_SLV_REG0_OFFSET, reset_data.bitsData);
+    FILAVG_mWriteReg(XPAR_FILAVG_0_S_AXI_BASEADDR, FILAVG_S_AXI_SLV_REG3_OFFSET, enable_data.bitsData);
+    usleep(5000);
 
     while(1){
         //setear enable
@@ -60,10 +73,12 @@ int main (void) {
         FILAVG_mWriteReg(XPAR_FILAVG_0_S_AXI_BASEADDR, FILAVG_S_AXI_SLV_REG3_OFFSET, enable_data.bitsData);
 
         //enviar un dato
-        input_data.signal = seno16[i];
+        input_data.signal = (uint16_t)mul*seno16[i];
         FILAVG_mWriteReg(XPAR_FILAVG_0_S_AXI_BASEADDR, FILAVG_S_AXI_SLV_REG1_OFFSET, input_data.signalData);
 
-        usleep(5000);
+        //sacar dato
+        uint32_t res = FILAVG_mReadReg(XPAR_FILAVG_0_S_AXI_BASEADDR, FILAVG_S_AXI_SLV_REG2_OFFSET);
+        xil_printf("Valor RMS: %d V\r\n", res/100);
 
         //borrar enable
         enable_data.b0 = 0;
@@ -71,14 +86,18 @@ int main (void) {
 
         i = (i + 1) % 16;
 
-        //sacar dato
-        uint32_t res = FILAVG_mReadReg(XPAR_FILAVG_0_S_AXI_BASEADDR, FILAVG_S_AXI_SLV_REG2_OFFSET);
-        xil_printf("Valor RMS de senial: %d\r\n", res);
+        count--;
+        if(count==0){
+        	count = 20;
+        	reset_data.b0 = 1;
+        	FILAVG_mWriteReg(XPAR_FILAVG_0_S_AXI_BASEADDR, FILAVG_S_AXI_SLV_REG0_OFFSET, reset_data.bitsData);
+        	usleep(5000);
+        	reset_data.b0 = 0;
+        	FILAVG_mWriteReg(XPAR_FILAVG_0_S_AXI_BASEADDR, FILAVG_S_AXI_SLV_REG0_OFFSET, reset_data.bitsData);
+        	xil_printf("Reset...");
+        }
 
         sleep(1);
 
     }
-    
-
-
 }
